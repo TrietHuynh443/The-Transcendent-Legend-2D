3a2f5a669ee24e34db6eb4cf7ad53aab3754f23e
Huỳnh Long <106131819+huynhlonguni@users.noreply.github.com>
2024-10-26 13:46:45 +0700

    Add Jump Animation (#13)
    * feat: add jump animation
    
    * feat: move player to another scene
    
    * feat: apply scene changes

--- Assets/_StateMachine/PlayerController.cs
+++ Assets/_StateMachine/PlayerController.cs
@@ -9,8 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _speed;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private int _maxJump;
 
     private float _horizontalInput;
+    private float _currentJumpSpeed;
+    private int _jumpCount; 
     private SpriteRenderer _spriteRenderer;
 
     // Start is called before the first frame update
@@ -25,6 +29,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
+
+        float velocityY = _rigidbody.velocity.y;
+
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            && (_jumpCount < _maxJump))
+        {
+            _currentJumpSpeed = _jumpSpeed;
+            _animator.SetBool("Jumping", true);
+            _jumpCount++;
+        }
+
+        if (velocityY == 0 && _currentJumpSpeed != _jumpSpeed)
+        {
+            _animator.SetBool("Jumping", false);
+            _jumpCount = 0;
+        }
     }
 
     void FixedUpdate()
@@ -36,7 +56,8 @@ public class PlayerController : MonoBehaviour
             transform.SetPositionAndRotation(transform.position, Quaternion.Euler(new Vector3(transform.rotation.x, newY)));
         }
         
-        _rigidbody.velocity = new Vector2(_horizontalInput * _speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_horizontalInput * _speed, _rigidbody.velocity.y + _currentJumpSpeed);
         _animator?.SetFloat("Move", abs);
+        _currentJumpSpeed = 0;
     }
 }

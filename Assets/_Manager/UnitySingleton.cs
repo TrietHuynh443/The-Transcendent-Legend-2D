e3e4866fec0a1e0aeb225ef70d66ce8530433a88
TrietHuynh443 <triethuynh4403@gmail.com>
2024-12-11 22:05:19 +0700

    tst

--- Assets/_Manager/UnitySingleton.cs
+++ Assets/_Manager/UnitySingleton.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Unity.IO.LowLevel.Unsafe;
-using Unity.VisualScripting;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class UnitySingleton<T> : MonoBehaviour where T : UnitySingleton<T>
